9515939832ac3d542ec05f95a04c78e123addec3
Diego Gonzalez <dihegobu@gmail.com>
2026-03-24 12:17:42 -0700

    Remove db.create_all() from app context
    Removed database creation from app context.

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from prepdocente import create_app
-from prepdocente.extensions import db
 
 app = create_app()
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
