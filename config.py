5f86c5f703a3a667b755672c39bb8ed819a7680f
Diego Gonzalez <dihegobu@gmail.com>
2026-03-24 14:08:06 -0700

    Update config.py

--- config.py
+++ config.py
@@ -2,25 +2,13 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
 class Config:
-
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-cambiar")
-
     db_url = os.environ.get("DATABASE_URL")
 
     if db_url:
-        # Para Render / Postgres
-        SQLALCHEMY_DATABASE_URI = db_url.replace(
-            "postgres://",
-            "postgresql://",
-            1
-        )
+        SQLALCHEMY_DATABASE_URI = db_url.replace("postgres://", "postgresql://", 1)
     else:
-        # Para local SQLite
-        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
-            BASE_DIR,
-            "prepdocente.db"
-        )
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "prepdocente.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
