64f65a40dc1dbe9376a7c33f71ec3fde2331394d
Diego Gonzalez <dihegobu@gmail.com>
2026-03-24 11:58:13 -0700

    Initialize database within app context

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from prepdocente import create_app
+from prepdocente.extensions import db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
